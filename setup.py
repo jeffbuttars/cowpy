df89f4ba3e52cbfe6df30e4d3351d2ec300f381e
Jeff Buttars <jeffbuttars@gmail.com>
2013-03-01 13:29:06 -0700

    Adding a setup.py layout.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='Cowpy',
+      description="A cowsay clone for python in one file.",
+      author="Jeff Buttars",
+      author_email="jeffbuttars@gmail.com",
+      url="https://github.com/jeffbuttars/cowpy",
+      version='0.9',
+      packages=['cowpy']
+      )
