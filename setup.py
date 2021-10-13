3eeb305f2cc761abc5799c7353f1f7b133c66197
Jeff Buttars <jeffbuttars@gmail.com>
2021-10-13 10:27:07 -0600

    Add license and license_files keys into setup() call

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@
 
 from setuptools import setup
 
-__version__ = '1.1.0'
+__version__ = "1.1.0"
 
-setup(name='cowpy',
+setup(name="cowpy",
       description="A cowsay clone for python in one file.",
       author="Jeff Buttars",
       author_email="jeffbuttars@gmail.com",
       url="https://github.com/jeffbuttars/cowpy",
       version=__version__,
-      packages=['cowpy'],
-      scripts=['scripts/cowpy', 'scripts/cowpy.bat']
+      packages=["cowpy"],
+      license="MIT",
+      license_files=("LICENSE.md",),
+      scripts=["scripts/cowpy", "scripts/cowpy.bat"]
       )
