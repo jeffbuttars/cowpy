9ffed3cc7df941771982f9ebc20860752b0d5c54
Jeff Buttars <jeffbuttars@gmail.com>
2021-12-08 08:10:30 -0700

    Add  section to  and clean up  a little bit

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup  # type: ignore
 
 __version__ = "1.1.2"
 
-setup(name="cowpy",
-      description="A cowsay clone for python in one file.",
-      author="Jeff Buttars",
-      author_email="jeffbuttars@gmail.com",
-      url="https://github.com/jeffbuttars/cowpy",
-      version=__version__,
-      packages=["cowpy"],
-      license="Apache License 2.0",
-      license_files=("LICENSE.md",),
-      scripts=["scripts/cowpy", "scripts/cowpy.bat"]
-      )
+setup(
+    name="cowpy",
+    description="A cowsay clone for python in one file.",
+    author="Jeff Buttars",
+    author_email="jeffbuttars@gmail.com",
+    url="https://github.com/jeffbuttars/cowpy",
+    python_requires=">=3.0",
+    version=__version__,
+    packages=["cowpy"],
+    license="Apache License 2.0",
+    license_files=("LICENSE.md",),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License"
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    scripts=["scripts/cowpy", "scripts/cowpy.bat"],
+)
